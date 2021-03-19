98fcbb4b1c36222633d68df9050fc20a944bca88
yavarguliyev <yavarrg@code.edu.az>
2021-03-20 00:18:50 +0400

    client&expiration test

--- server/expiration/src/index.ts
+++ server/expiration/src/index.ts
@@ -2,12 +2,16 @@ import { natsWrapper } from './nats-wrapper';
 import { OrderCreatedListener } from './events/listeners/order-created.listener';
 
 const start = async () => {
+  console.log('Starting...');
+
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
+
   if (!process.env.NATS_URL) {
     throw new Error('NATS_URL must be defined');
   }
+
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error('NATS_CLUSTER_ID must be defined');
   }
