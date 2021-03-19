5dba98c2223e5ba6c1bdd9d87b4e0f6fbce01c73
yavarguliyev <yavarrg@code.edu.az>
2021-03-20 00:27:58 +0400

    ticket, payments & orders test added

--- server/orders/src/index.ts
+++ server/orders/src/index.ts
@@ -7,18 +7,24 @@ import { TicketUpdatedListener } from './events/listeners/ticket-updated-listene
 import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
 
 const start = async () => {
+  console.log('Starting......');
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+
   if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
+
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
+
   if (!process.env.NATS_URL) {
     throw new Error('NATS_URL must be defined');
   }
+
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error('NATS_CLUSTER_ID must be defined');
   }
